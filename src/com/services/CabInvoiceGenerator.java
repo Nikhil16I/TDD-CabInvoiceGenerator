222884c241972554ef88732db713a106a875b0ba
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-22 23:21:33 +0530

    Step 3 - Enhanced Invoice

--- src/com/services/CabInvoiceGenerator.java
+++ src/com/services/CabInvoiceGenerator.java
@@ -1,13 +1,17 @@
-package com.CabInvoiceGenerator;
+package com.services;
+
 
 import java.util.Iterator;
+
+import com.entity.InvoiceData;
+import com.entity.Rides;
 
 public class CabInvoiceGenerator {
 	
 	static int  MinimumFare = 5;
 
 	static final double CostPerKM = 10.0;
-	static final double CostPerMIN = 1;
+	static final int CostPerMIN = 1;
 	
 	public double CalculateFare(double Distance, double Time) {
 
@@ -19,7 +23,7 @@ public class CabInvoiceGenerator {
 		return TotalFare;
 	}
 
-	public double CalculateFareMultipleRides(Rides[] rides) {
+	public static InvoiceData CalculateFareMultipleRides(Rides[] rides) {
 		// TODO Auto-generated method stub
 		double TotalFare =0;
 		for (int i = 0; i < rides.length; i++) {
@@ -27,6 +31,7 @@ public class CabInvoiceGenerator {
 			double newFare=((rides2.getDistance()*CostPerKM)+(rides2.getTime()*CostPerMIN));
 			TotalFare += newFare;
 		} 
-		return TotalFare;
+		return new InvoiceData(rides.length,TotalFare);
 	}
 }
+
